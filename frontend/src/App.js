8cb63dfc582790045e47345d6de3cb4896b7e790
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-02-24 18:28:49 +0530

    Added Sample Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
         <a href="/">
-          <span className="logoFirst">Ecom</span>-Bidding
+          <span className="logoFirst">Ecom</span>Bidding
         </a>
       </header>
 
-      <main>Products come here</main>
+      <main>
+        <h2>Listed Products</h2>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="items" key={product.id}>
+              <a href={`/products/${product.id}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="items__info">
+                <a href={`/products/${product.id}`}>
+                  <p>{product.name}</p>
+                  <p>
+                    <small>₹</small>
+                    {product.price}
+                  </p>
+                </a>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,55 @@
+// returns data for products
+const data = {
+  products: [
+    {
+      name: 'Fujifilm Instax Mini 9 Instant Camera',
+      id: 'Fujifilm-Instax-Mini-9-Instant-Camera', // id: for url compatibility
+      description: 'premium quality camera for capturing your best moments',
+      category: 'Electronics',
+      image: './images/Fujifilm.jpg',
+      price: 4999,
+      rating: 4.5,
+      brand: 'Fujifilm',
+      stock: 10,
+      reviews: 5,
+    },
+    {
+      name: 'Fujifilm Instax Mini 9 Instant Camera',
+      id: 'Fujifilm-Instax-Mini-9-Instant-Camera-2', // id: for url compatibility
+      description: 'premium quality camera for capturing your best moments',
+      category: 'Electronics',
+      image: './images/Fujifilm.jpg',
+      price: 4999,
+      rating: 4.5,
+      brand: 'Fujifilm',
+      stock: 10,
+      reviews: 5,
+    },
+    {
+      name: 'Fujifilm Instax Mini 9 Instant Camera',
+      id: 'Fujifilm-Instax-Mini-9-Instant-Camera-3', // id: for url compatibility
+      description: 'premium quality camera for capturing your best moments',
+      category: 'Electronics',
+      image: './images/Fujifilm.jpg',
+      price: 4999,
+      rating: 4.5,
+      brand: 'Fujifilm',
+      stock: 10,
+      reviews: 5,
+    },
+    {
+      name: 'Fujifilm Instax Mini 9 Instant Camera',
+      id: 'Fujifilm-Instax-Mini-9-Instant-Camera-4', // id: for url compatibility
+      description: 'premium quality camera for capturing your best moments',
+      category: 'Electronics',
+      image: './images/Fujifilm.jpg',
+      price: 4999,
+      rating: 4.5,
+      brand: 'Fujifilm',
+      stock: 10,
+      reviews: 5,
+    },
+  ],
+};
+
+export default data;
