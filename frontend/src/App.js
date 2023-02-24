ffecb52597f3b3e6b07c1403af255531ae48776d
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-02-24 17:19:18 +0530

    Started working on Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <header>
+        <a href="/">
+          <span className="logoFirst">Ecom</span>-Bidding
         </a>
       </header>
+
+      <main>Products come here</main>
     </div>
   );
 }
