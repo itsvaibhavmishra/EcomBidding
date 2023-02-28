4ac78aded6a164f21288add14d3dc8c1477a72f8
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-02-28 17:04:00 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -23,25 +23,6 @@ mongoose
 // returns list of products for this api
 app.use('/api/products', productRouter);
 
-// :url is the product url user requested
-app.get('/api/products/url/:url', (req, res) => {
-  const product = data.products.find((item) => item.url === req.params.url);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((item) => item._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
