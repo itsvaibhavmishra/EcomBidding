690ceff38d77c5a4c78edf9202f179d9b15b6c33
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-03-05 23:01:49 +0530

    Fixed frontend issues on Profile

--- frontend/src/Pages/ProfilePage/ProfilePage.js
+++ frontend/src/Pages/ProfilePage/ProfilePage.js
@@ -7,7 +7,9 @@ function ProfilePage() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
   const [name, setName] = useState(userInfo.name);
-  const [email, setEmail] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const submitHandler = async () => {};
   return (
@@ -16,92 +18,97 @@ function ProfilePage() {
         <title>Profile-EcomBidding</title>
       </Helmet>
       {/* component */}
-      <div class="flex min-h-screen flex-col justify-center overflow-hidden bg-gray-100 py-6 sm:py-12">
-        <div class="relative py-3 sm:mx-auto sm:max-w-xl">
-          <div class="animate-spin-custom absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-300 to-cyan-500 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl"></div>
-          <div class="relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20">
-            <div class="mx-auto max-w-md">
+      <div className="flex min-h-fit flex-col justify-center overflow-hidden bg-gray-100 py-6 sm:py-12">
+        <div className="relative py-3 sm:mx-auto sm:max-w-xl">
+          <div className="animate-spin-custom absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-300 to-cyan-500 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl"></div>
+          <div className="relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="mx-auto max-w-md">
               <div>
-                <h1 class="text-2xl font-semibold">
+                <h1 className="text-2xl font-semibold">
                   User Profile - Edit your profile
                 </h1>
               </div>
-              <div class="divide-y divide-gray-200">
+              <div className="divide-y divide-gray-200">
                 <form
-                  class="space-y-4 py-8 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7"
+                  className="space-y-4 py-8 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7"
                   onSubmit={submitHandler}
                 >
-                  <div class="relative">
+                  <div className="relative">
                     <input
-                      autocomplete="off"
+                      autoComplete="off"
                       id="name"
                       name="name"
                       type="text"
-                      class="focus:borer-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none"
+                      className="focus:border-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none text-base"
                       placeholder="Name"
                       value={name}
                       onChange={(e) => setName(e.target.value)}
                       required
                     />
                     <label
-                      for="name"
-                      class="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
+                      htmlFor="name"
+                      className="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
                     >
                       Name
                     </label>
                   </div>
-                  <div class="relative">
+                  <div className="relative">
                     <input
-                      autocomplete="off"
+                      autoComplete="off"
                       id="email"
                       name="email"
                       type="email"
-                      class="focus:borer-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none"
+                      className="focus:border-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none text-base"
                       placeholder="Email address"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
                     />
                     <label
-                      for="email"
-                      class="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
+                      htmlFor="email"
+                      className="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
                     >
                       Email Address
                     </label>
                   </div>
-                  <div class="relative">
+                  <div className="relative">
                     <input
-                      autocomplete="off"
+                      autoComplete="off"
                       id="password"
                       name="password"
                       type="password"
-                      class="focus:borer-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none"
+                      className="focus:border-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none text-base"
                       placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                     <label
-                      for="password"
-                      class="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
+                      htmlFor="password"
+                      className="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
                     >
                       Password
                     </label>
                   </div>
-                  <div class="relative">
+                  <div className="relative">
                     <input
-                      autocomplete="off"
-                      id="password"
+                      autoComplete="off"
+                      id="confirmPassword"
                       name="password"
                       type="password"
-                      class="focus:borer-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none"
+                      className="focus:border-rose-600 peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none text-base"
                       placeholder="Password"
+                      onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                     <label
-                      for="password"
-                      class="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
+                      htmlFor="confirmPassword"
+                      className="peer-placeholder-shown:text-gray-440 absolute left-0 -top-3.5 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
                     >
                       Confirm Password
                     </label>
                   </div>
-                  <div class="relative">
+                  <div className="relative">
                     <button
                       type="submit"
-                      class="rounded-md bg-cyan-500 px-2 py-1 text-white duration-200 hover:bg-cyan-600"
+                      className="rounded-md bg-cyan-500 px-2 py-1 text-white duration-200 hover:bg-cyan-600"
                     >
                       Update
                     </button>
