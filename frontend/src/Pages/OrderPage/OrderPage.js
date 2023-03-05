b03a258694bc99aeaec500692d60fec1d0404e45
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-03-05 23:59:27 +0530

    Local Storage added for Profile and fixed issue in Order Page

--- frontend/src/Pages/OrderPage/OrderPage.js
+++ frontend/src/Pages/OrderPage/OrderPage.js
@@ -183,7 +183,7 @@ export default function OrderPage() {
                   <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
                     <div className="w-full flex flex-col justify-start items-start space-y-8">
                       <h3 className="text-xl light:text-white font-semibold leading-6 text-gray-800">
-                        <Link to={`/product/${item.url}`}>{item.name}</Link>
+                        <Link to={`/products/${item.url}`}>{item.name}</Link>
                       </h3>
                     </div>
                     <div className="flex justify-between space-x-8 items-start w-full">
--- frontend/src/Pages/ProfilePage/ProfilePage.js
+++ frontend/src/Pages/ProfilePage/ProfilePage.js
@@ -1,7 +1,23 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useReducer, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Store } from '../../Store';
+import { getError } from '../../utils';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 import './ProfilePage.css';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_REQUEST':
+      return { ...state, loadingUpdate: true };
+    case 'UPDATE_SUCCESS':
+      return { ...state, loadingUpdate: false };
+    case 'UPDATE_FAIL':
+      return { ...state, loadingUpdate: false };
+    default:
+      return state;
+  }
+};
 
 function ProfilePage() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -11,7 +27,37 @@ function ProfilePage() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const submitHandler = async () => {};
+  const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
+    loadingUpdate: false,
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/users/profile`,
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: 'UPDATE_SUCCESS',
+      });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      toast.success('Information Updated Successfully 👾');
+    } catch (err) {
+      dispatch({
+        type: 'UPDATE_FAIL',
+      });
+      toast.error(getError(err));
+    }
+  };
   return (
     <div>
       <Helmet>
