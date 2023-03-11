821f3151c92037e67549382074855383cbd82054
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-03-11 15:09:10 +0530

    Changed loading color

--- frontend/src/Components/Loading/Loading.js
+++ frontend/src/Components/Loading/Loading.js
@@ -6,7 +6,7 @@ function Loading() {
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
         <div className="relative">
           <div
-            className="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-cyan-500"
+            className="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-cyan-100"
             role="status"
           >
             <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
