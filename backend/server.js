f5768ba4fa2e8b780d5349a272904a49b518d31f
Vaibhaw Mishra <51448116+itsvaibhavmishra@users.noreply.github.com>
2023-04-10 23:09:41 +0530

    Deploying Backend

--- backend/server.js
+++ backend/server.js
@@ -45,12 +45,6 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/auctions', auctionRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
